c220212f4ece5191a278278f5f62ed553ae73518
Nishal Tamse <nishaltamse20@gmail.com>
2025-06-15 15:06:28 +0000

    Datatypes

---
+++ 01_basics/02_dataTypes.js
@@ -0,0 +1,28 @@
+"use strict"; //treat all code as newer version of js
+
+//alert("Hello Nishal!");  we are using nodejs,not browser
+
+console.log(3 
+    +
+      2);// code readilitiy is important
+
+// console.log(3 + 2); // this is also correct
+
+console.log("Hello Nishal!");
+
+let name = "Nishal";
+let age = 20;
+let isStudent = true;
+
+//number => 2 to pwr 53
+//bigint 
+//string = ""
+//boolean = true/false
+//null = empty value
+//undefined = variable not defined
+//symbol = unique value
+
+
+//OBJECTS
+
+console.log(typeof null); 
