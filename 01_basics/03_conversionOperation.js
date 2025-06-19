9c3e4d4109ee4eeb977b2e526890f47dcb50ebfe
Nishal Tamse <nishaltamse20@gmail.com>
2025-06-19 14:37:48 +0000

    operators and increments learning !!

--- 01_basics/03_conversionOperation.js
+++ 01_basics/03_conversionOperation.js
@@ -1,33 +1,70 @@
-let score = undefined
+// let score = undefined
 
-console.log(typeof score)
-console.log(typeof(score));
+// console.log(typeof score)
+// console.log(typeof(score));
 
-let valueInNumber = Number(score)
-console.log(valueInNumber);
+// let valueInNumber = Number(score)
+// console.log(valueInNumber);
 
-console.log(typeof valueInNumber);
-
-
-//"33" -> 33
-//"33qwe" -> NaN
-// true -> 1, false -> 0
+// console.log(typeof valueInNumber);
 
 
-let isloggedIn = 1
+// //"33" -> 33
+// //"33qwe" -> NaN
+// // true -> 1, false -> 0
 
-let booleanIsLoggedIn = Boolean(isloggedIn)
-console.log(booleanIsLoggedIn);
 
-/*
-1 => true; 0 => false
-"" => false
-"Nishal" => true
-*/
+// let isloggedIn = 1
 
-let someNumber = 20
+// let booleanIsLoggedIn = Boolean(isloggedIn)
+// console.log(booleanIsLoggedIn);
 
-let stringNumber = String(someNumber);
-console.log(stringNumber);
-console.log(typeof stringNumber);
+// /*
+// 1 => true; 0 => false
+// "" => false
+// "Nishal" => true
+// */
+
+// let someNumber = 20
+
+// let stringNumber = String(someNumber);
+// console.log(stringNumber);
+// console.log(typeof stringNumber);
+
+
+// *******************************OPERATION*************************************
+
+let value = 3
+let negValue = -value
+//console.log(negValue);
+
+
+//console.log(2+2)
+//console.log(2-2)
+//console.log(2*2)
+//console.log(2**3) -> power
+//console.log(2/4)
+//console.log(2%3) => modulus
+
+
+let str1 = "Hello"
+let str2 = " Nishal"
+let str3 = str1 + str2
+console.log(str3);
+
+// console.log("1" + 2);
+// console.log(1 + "2");
+// console.log("1" + 2 + 2);
+// console.log(1 + 2 + "2");
+
+// console.log((3 + 4) * 2);
+
+// let num1, num2, num3;
+// num1 =  num2 = num3 = 2+1 ==> not a suitable way to write a code
+
+let GameCounter = 21
+++GameCounter;
+console.log(GameCounter); 
+
+// Operator prescendence and increment is important  ;learn post, Prefix increment
 
