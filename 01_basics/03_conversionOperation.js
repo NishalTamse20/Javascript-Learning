c593629b03811007313c2d9b588c737307d940bd
Nishal Tamse <nishaltamse20@gmail.com>
2025-06-19 14:04:50 +0000

    Conversion Operation Learning!!!

---
+++ 01_basics/03_conversionOperation.js
@@ -0,0 +1,33 @@
+let score = undefined
+
+console.log(typeof score)
+console.log(typeof(score));
+
+let valueInNumber = Number(score)
+console.log(valueInNumber);
+
+console.log(typeof valueInNumber);
+
+
+//"33" -> 33
+//"33qwe" -> NaN
+// true -> 1, false -> 0
+
+
+let isloggedIn = 1
+
+let booleanIsLoggedIn = Boolean(isloggedIn)
+console.log(booleanIsLoggedIn);
+
+/*
+1 => true; 0 => false
+"" => false
+"Nishal" => true
+*/
+
+let someNumber = 20
+
+let stringNumber = String(someNumber);
+console.log(stringNumber);
+console.log(typeof stringNumber);
+
